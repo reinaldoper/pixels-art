5c8b459429a1c51c7e7b5dfaecd3a9c5e6e6b054
Reinaldo <reinaldoper83@gmail.com>
2022-05-25 15:28:42 -0300

    paleta de cores

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 //Primeiro requisito
-let titleH1 = document.querySelector("body");
-let tagH1 = document.createElement("h1");
+const titleH1 = document.querySelector("body");
+const tagH1 = document.createElement("h1");
 tagH1.innerHTML = "Paleta de Cores";
 tagH1.id = "title";
 titleH1.appendChild(tagH1);
 
-//
+/* //Segundo requisito
+let paletaColors = document.querySelector("title");
+let palet = document.createElement("div");
+palet.id = "color-palette";
+ */
