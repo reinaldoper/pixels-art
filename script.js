a33466a9837e64764d1e6a8e63191b621068c971
Reinaldo <reinaldoper83@gmail.com>
2022-05-25 14:56:58 -0300

    Primeiro requisito: tag h1 e título

---
+++ script.js
@@ -0,0 +1,9 @@
+
+//Primeiro requisito
+let titleH1 = document.querySelector("body");
+let tagH1 = document.createElement("h1");
+tagH1.innerHTML = "Paleta de Cores";
+tagH1.id = "title";
+titleH1.appendChild(tagH1);
+
+//
